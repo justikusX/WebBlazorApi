13b2f6e2811c98c7ef7795b5d6864bbf5fca10c3
JustikusX <JustikusX@209-CH-2>
2025-12-26 14:30:41 +0200

    asd

--- Domain/Project.cs
+++ Domain/Project.cs
@@ -8,6 +8,9 @@ public sealed class Project
     public string Description { get; set; } = null!;
     public ProjectStatus Status { get; set; }
 
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Cost { get; set; }
     public DateTime StartDateUtc { get; set; }
     public DateTime DueDateUtc { get; set; }
     public decimal PlannedCost { get; set; }
